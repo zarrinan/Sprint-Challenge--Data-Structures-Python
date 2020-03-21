81766cbbb8ea33338cdd658888905801823d1078
Niyazova <zniyazov@NYMIT058770.fios-router.home>
2020-03-21 12:43:28 -0400

    Add buffer solution

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,16 +6,32 @@ class RingBuffer:
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+        self.track = 0
 
     def append(self, item):
-        pass
+        if self.storage.length < self.capacity:  # If buffer isn't full, add to head
+            self.storage.add_to_tail(item)
+            self.track += 1
+        else:  # If it's full remove in order
+            cur = self.storage.head
+            for _ in range(self.track % self.capacity):
+                cur = cur.next
+            cur.value = item
+            self.track += 1
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-
+        item = self.storage.head
+        if self.storage.length < 1:
+            return list_buffer_contents
+        else:
+            while item.next:
+                list_buffer_contents.append(item.value)
+                item = item.next
+            list_buffer_contents.append(item.value)
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
