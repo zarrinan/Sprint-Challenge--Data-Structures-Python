8c647e1a229693b6b9a98c15da7cf2f9382d63ab
Niyazova <zniyazov@NYMIT058770.fios-router.home>
2020-03-21 13:22:10 -0400

    Complete sc

--- names/names.py
+++ names/names.py
@@ -1,4 +1,6 @@
 import time
+from bst import BinarySearchTree
+
 
 start_time = time.time()
 
@@ -13,16 +15,32 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)  # 5.071461915969849 seconds
+
+# Binary search tree would be an optimal data structure to solve this problem
+bst = BinarySearchTree(names_1[0])
+
+# Insert names from the names_1 into the bst
+for name in names_1[1:]:
+    bst.insert(name)
+
+# Check if the bst contains names from the names_2
+for name in names_2:
+    if bst.contains(name):
+        # If there are same names, append to the dublicates
+        duplicates.append(name)  # 0.0876319408416748 seconds
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# It would be sets, completed in 0.003823995590209961 seconds, faster than BST
+duplicates = set(names_1).intersection(names_2)
